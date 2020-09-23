4d03b3e60e04eded44bafcc3e952cc840c294ef8
AryaveerSR <AryaveerSR@users.noreply.github.com>
2020-09-23 10:58:02 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             // *** Things to Do if Page Loads **
 
             var doIt = () => {
-                getID('mainLogo').src = store.get('imgTag') ? store.get('imgTag') : 'https://cdn.discordapp.com/attachments/747410238944051271/751466328262443169/FIXED.png';
+                getID('mainLogo').src = store.get('imgTag') || 'https://cdn.discordapp.com/attachments/747410238944051271/751466328262443169/FIXED.png';
                 getID('mapInfoHolder').children[3].insertAdjacentHTML('beforeend', '<a class="terms" href="/">&nbsp;QuickJoin&nbsp;</a><div style="font-size:20px;color:#fff;display:inline-block;">|</div>')
                 getID('menuClassContainer').insertAdjacentHTML('beforeend', '<div id="scopeSelect customizeButton" class="button bigShadowT mycustomButton" onclick="window.scopes()" onmouseenter="playTick()">Scopes</div>');
                 getID('menuClassContainer').insertAdjacentHTML('beforeend', '<div id="scopeSelect customizeButton" class="button bigShadowT mycustomButton" onclick="window.Css()" onmouseenter="playTick()">RS</div>');
