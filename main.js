4fdf4cad68eb4824c0f6cef7828f08d8c671d49b
AryaveerSR <AryaveerSR@users.noreply.github.com>
2020-09-29 21:37:12 +0530

    Anooother Resource Swapper BUg Fixed

--- main.js
+++ main.js
@@ -5,12 +5,12 @@
 
 // *** The Modules ***
 const {
-  screen,
-  app,
-  BrowserWindow,
-  shell,
-  globalShortcut,
-  ipcMain,
+    screen,
+    app,
+    BrowserWindow,
+    shell,
+    globalShortcut,
+    ipcMain,
 } = require("electron");
 var { cpus } = require("os");
 const { format } = require("url");
@@ -49,198 +49,198 @@ app.commandLine.appendSwitch("disable-logging");
 app.commandLine.appendSwitch("disable-web-security");
 app.commandLine.appendSwitch("webrtc-max-cpu-consumption-percentage=100");
 if (cpus()[0].model.includes("AMD")) {
-  app.commandLine.appendSwitch("enable-zero-copy");
+    app.commandLine.appendSwitch("enable-zero-copy");
 }
 
 var win = null;
 
 function createGameWindow() {
-  // *** Create the Game Window ***
+    // *** Create the Game Window ***
 
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-  win = new BrowserWindow({
-    icon: `${__dirname}/assets/icon/icon.ico`,
-    height: Math.round(height * 0.75),
-    width: Math.round(width * 0.75),
-    backgroundColor: "#000000",
-    webPreferences: {
-      nodeIntergration: true,
-      preload: `${__dirname}/preload.js`,
-      webSecurity: false,
-    },
-  });
-
-  win.webContents.openDevTools();
-
-  // *** If New window is Social ***
-
-  win.webContents.on(
-    "new-window",
-    (event, url, frameName, disposition, options) => {
-      initWin(event, url, frameName, disposition, options);
-    }
-  );
-
-  function initWin(event, url, frameName, disposition, options) {
-    if (!url) return;
-    if (!isSocial(url)) {
-      event.preventDefault();
-      shell.openExternal(url);
-      return;
-    } else {
-      event.preventDefault();
-      const newWin = new BrowserWindow({
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    win = new BrowserWindow({
+        icon: `${__dirname}/assets/icon/icon.ico`,
+        height: Math.round(height * 0.75),
         width: Math.round(width * 0.75),
-        height: Math.round(height * 0.8),
-        webContents: options.webContents,
-        show: false,
-        backgroundColor: "#131313",
+        backgroundColor: "#000000",
         webPreferences: {
-          nodeIntergration: true,
-          preload: `${__dirname}/social.js`,
-          webSecurity: false,
+            nodeIntergration: true,
+            preload: `${__dirname}/preload.js`,
+            webSecurity: false,
         },
-      });
+    });
 
-      globalShortcut.register("F11", () => {
-        win.setSimpleFullScreen(!win.isSimpleFullScreen());
-      });
-      globalShortcut.register("F3", () => {
-        win.webContents.send("home");
-      });
-      globalShortcut.register("F5", () => {
-        app.relaunch();
-        app.quit();
-      });
-      globalShortcut.register("F7", () => {
-        if (devTools) win.webContents.openDevTools();
-      });
-      if (!options.webContents) {
-        newWin.loadURL(url);
+    win.webContents.openDevTools();
 
-        newWin.webContents.once("dom-ready", function () {
-          setTimeout(() => {
-            newWin.show();
-          }, 600);
-        });
-      }
-      event.newGuest = newWin;
-      newWin.removeMenu();
-      if (devTools) win.webContents.openDevTools();
-      newWin.webContents.on(
+    // *** If New window is Social ***
+
+    win.webContents.on(
         "new-window",
         (event, url, frameName, disposition, options) => {
-          initWin(event, url, frameName, disposition, options);
+            initWin(event, url, frameName, disposition, options);
         }
-      );
-    }
-  }
-
-  function isSocial(rawUrl) {
-    return new URL(rawUrl).pathname == "/social.html" ? true : false;
-  }
-
-  // *** Set the Shortcuts ***
-
-  globalShortcut.register("F11", () => {
-    win.setSimpleFullScreen(!win.isSimpleFullScreen());
-  });
-  globalShortcut.register("F3", () => {
-    win.webContents.send("home");
-  });
-  globalShortcut.register("F5", () => {
-    app.relaunch();
-    app.quit();
-  });
-  globalShortcut.register("F7", () => {
-    if (devTools) win.webContents.openDevTools();
-  });
-
-  globalShortcut.register("Esc", () => {
-    win.webContents.send("Escape");
-  });
-  if (devTools) win.webContents.openDevTools();
-  win.setSimpleFullScreen(fullscreenOnload);
-  win.loadURL("https://krunker.io");
-  win.removeMenu();
-
-  // *** Resource Swapper Code ***
-
-  let sf = `${app.getPath("documents")}/ZenoSwapper/`;
-
-  try {
-    mkdir(sf, { recursive: true }, (e) => {});
-  } catch (e) {}
-  let s = { fltr: { urls: [] }, fls: {} };
-  const afs = (dir, fileList = []) => {
-    readdirSync(dir).forEach((file) => {
-      var fp;
-      if (dir.endsWith("/")) {
-        fp = dir + file;
-      } else {
-        fp = dir + "/" + file;
-      }
-      if (statSync(fp).isDirectory()) {
-        if (!/\\(docs)$/.test(fp)) afs(fp);
-      } else {
-        if (!/\.(html|js)/g.test(file)) {
-          let k =
-            "*://krunker.io" + fp.replace(sf, "").replace(/\\/g, "/") + "*";
-          s.fltr.urls.push(k);
-          s.fls[k.replace(/\*/g, "")] = format({
-            pathname: fp,
-            protocol: "file:",
-            slashes: true,
-          });
-        }
-      }
-    });
-  };
-  afs(sf);
-  if (s.fltr.urls.length) {
-    win.webContents.session.webRequest.onBeforeRequest(
-      s.fltr,
-      (details, callback) => {
-        callback({
-          cancel: false,
-          redirectURL:
-            s.fls[details.url.replace(/https|http|(\?.*)|(#.*)/gi, "")] ||
-            details.url,
-        });
-      }
     );
-  }
-  win.webContents.on("did-finish-load", () => {
-    win.setTitle("Krunker - Zeno Client");
-  });
+
+    function initWin(event, url, frameName, disposition, options) {
+        if (!url) return;
+        if (!isSocial(url)) {
+            event.preventDefault();
+            shell.openExternal(url);
+            return;
+        } else {
+            event.preventDefault();
+            const newWin = new BrowserWindow({
+                width: Math.round(width * 0.75),
+                height: Math.round(height * 0.8),
+                webContents: options.webContents,
+                show: false,
+                backgroundColor: "#131313",
+                webPreferences: {
+                    nodeIntergration: true,
+                    preload: `${__dirname}/social.js`,
+                    webSecurity: false,
+                },
+            });
+
+            globalShortcut.register("F11", () => {
+                win.setSimpleFullScreen(!win.isSimpleFullScreen());
+            });
+            globalShortcut.register("F3", () => {
+                win.webContents.send("home");
+            });
+            globalShortcut.register("F5", () => {
+                app.relaunch();
+                app.quit();
+            });
+            globalShortcut.register("F7", () => {
+                if (devTools) win.webContents.openDevTools();
+            });
+            if (!options.webContents) {
+                newWin.loadURL(url);
+
+                newWin.webContents.once("dom-ready", function() {
+                    setTimeout(() => {
+                        newWin.show();
+                    }, 600);
+                });
+            }
+            event.newGuest = newWin;
+            newWin.removeMenu();
+            if (devTools) win.webContents.openDevTools();
+            newWin.webContents.on(
+                "new-window",
+                (event, url, frameName, disposition, options) => {
+                    initWin(event, url, frameName, disposition, options);
+                }
+            );
+        }
+    }
+
+    function isSocial(rawUrl) {
+        return new URL(rawUrl).pathname == "/social.html" ? true : false;
+    }
+
+    // *** Set the Shortcuts ***
+
+    globalShortcut.register("F11", () => {
+        win.setSimpleFullScreen(!win.isSimpleFullScreen());
+    });
+    globalShortcut.register("F3", () => {
+        win.webContents.send("home");
+    });
+    globalShortcut.register("F5", () => {
+        app.relaunch();
+        app.quit();
+    });
+    globalShortcut.register("F7", () => {
+        if (devTools) win.webContents.openDevTools();
+    });
+
+    globalShortcut.register("Esc", () => {
+        win.webContents.send("Escape");
+    });
+    if (devTools) win.webContents.openDevTools();
+    win.setSimpleFullScreen(fullscreenOnload);
+    win.loadURL("https://krunker.io");
+    win.removeMenu();
+
+    // *** Resource Swapper Code ***
+
+    let sf = `${app.getPath("documents")}/ZenoSwapper`;
+
+    try {
+        mkdir(sf, { recursive: true }, (e) => {});
+    } catch (e) {}
+    let s = { fltr: { urls: [] }, fls: {} };
+    const afs = (dir, fileList = []) => {
+        readdirSync(dir).forEach((file) => {
+            var fp;
+            if (dir.endsWith("/")) {
+                fp = dir + file;
+            } else {
+                fp = dir + "/" + file;
+            }
+            console.log(fp)
+            if (statSync(fp).isDirectory()) {
+                if (!/\\(docs)$/.test(fp)) afs(fp);
+            } else {
+                if (!/\.(html|js)/g.test(file)) {
+                    let k =
+                        "*://krunker.io" + fp.replace(sf, "").replace(/\\/g, "/") + "*";
+                    s.fltr.urls.push(k);
+                    s.fls[k.replace(/\*/g, "")] = format({
+                        pathname: fp,
+                        protocol: "file:",
+                        slashes: true,
+                    });
+                }
+            }
+        });
+    };
+    afs(sf);
+    if (s.fltr.urls.length) {
+        win.webContents.session.webRequest.onBeforeRequest(
+            s.fltr,
+            (details, callback) => {
+                callback({
+                    cancel: false,
+                    redirectURL: s.fls[details.url.replace(/https|http|(\?.*)|(#.*)/gi, "")] ||
+                        details.url,
+                });
+            }
+        );
+    }
+    win.webContents.on("did-finish-load", () => {
+        win.setTitle("Krunker - Zeno Client");
+    });
 }
 
 // *** Run the Main Function ***
 
 var init = () => {
-  createGameWindow();
+    createGameWindow();
 };
 
 app.whenReady().then(() => {
-  init();
+    init();
 
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) init();
-  });
+    app.on("activate", function() {
+        if (BrowserWindow.getAllWindows().length === 0) init();
+    });
 });
 
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") {
-    app.quit();
-    globalShortcut.unregisterAll();
-  }
+app.on("window-all-closed", function() {
+    if (process.platform !== "darwin") {
+        app.quit();
+        globalShortcut.unregisterAll();
+    }
 });
 
 ipcMain.on("restart-client", () => {
-  app.relaunch();
-  app.quit();
+    app.relaunch();
+    app.quit();
 });
 
 ipcMain.on("close-client", () => {
-  app.quit();
+    app.quit();
 });
