ba41c96c6c30da0d156890615cbf4e8c035e89fd
AryaveerSR <AryaveerSR@users.noreply.github.com>
2020-10-02 19:05:55 +0530

    dab

--- rich-presence.js
+++ rich-presence.js
@@ -21,14 +21,26 @@ function initDiscord() {
             state: "Started Playing",
             largeImageKey: "zeno_menu",
             largeImageText: "Zeno Client",
-            smallImageKey: "zeno_dev",
+            smallImageKey: "zeno_menu",
             smallImageText: `${discordClient.user.username}#${discordClient.user.discriminator}`,
         });
     });
 
-    discordClient.subscribe("ACTIVITY_INVITE", ({ secret }) => joinGame(secret));
+    /*discordClient.subscribe("ACTIVITY_INVITE", ({ secret }) => {
+        console.log(secret)
+        window.location.href = `https://krunker.io/?game=${secret}`;
+    });
 
-    discordClient.subscribe("ACTIVITY_JOIN_REQUEST", ({ user }) => handleJoinRequest(user));
+    discordClient.subscribe("ACTIVITY_JOIN_REQUEST", ({ user }) => {
+        var { id, username, discriminator, avatar } = user;
+        window.confirm(`${username}#${discriminator} wants to join your game!`) ?
+            () => {
+                discordClient.request("SEND_ACTIVITY_JOIN_INVITE", { user_id: id });
+            } :
+            () => {
+                discordClient.request("CLOSE_ACTIVITY_REQUEST", { user_id: id });
+            };
+    });*/
 
     setInterval(() => {
         updateDiscord();
@@ -52,7 +64,7 @@ function updateDiscord() {
 
         if (gameMode == undefined || mapName == undefined || className == undefined) { return console.log("Not Loaded") } else {
             discordClient.setActivity({
-                details: `Playing ${game}`,
+                details: `Playing ${gameMode}`,
                 state: `on ${map}`,
                 endTimestamp: Date.now() + time * 1000,
                 largeImageKey: "zeno_menu",
@@ -64,20 +76,6 @@ function updateDiscord() {
             });
         }
     }
-}
-
-function joinGame(secret) {
-    window.location.href = `https://krunker.io/?game=${secret}`;
-}
-
-function handleJoinRequest({ id, username, discriminator, avatar }) {
-    window.confirm(`${username}#${discriminator} wants to join your game!`) ?
-        () => {
-            discordClient.request("SEND_ACTIVITY_JOIN_INVITE", { user_id: id });
-        } :
-        () => {
-            discordClient.request("CLOSE_ACTIVITY_REQUEST", { user_id: id });
-        };
 }
 
 exports.initDiscord = initDiscord;
