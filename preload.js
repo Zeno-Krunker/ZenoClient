3da5deeb984e2fc7c09bbbb02982a7544a2addc7
TheUltimateKeval <32948647+TheUltimateKeval@users.noreply.github.com>
2020-10-04 22:27:28 +0530

    Moved badge code to a separate file. Made a new directory for features. Cleaned up the code for badges.

--- preload.js
+++ preload.js
@@ -12,38 +12,11 @@ const tmi = require('tmi.js');
 const { copy, copySync } = require("fs-extra");
 const Store = require("electron-store");
 const store = new Store();
-const rsData = require("./rsData.json");
-const { initDiscord } = require("./rich-presence");
-var badgeObj;
-fetch("https://zenoupdateapi.web.app/badgeData.json").then((resp) => resp.json()).then((data) => {
-    badgeObj = data;
-}).catch(error => console.error(error));
-var badges = ["verified", "content-creators", "dev", "vip", "gfx"];
-var badgeUrls = {
-    verified: {
-        url: "https://cdn.discordapp.com/attachments/747410238944051271/748572694705864896/badge-verified.png",
-    },
-    "content-creators": {
-        url: "https://cdn.discordapp.com/attachments/747410238944051271/748574740830093473/badge-cc.png",
-    },
-    dev: {
-        url: "https://cdn.discordapp.com/attachments/747410238944051271/748573364968226887/badge-dev.png",
-    },
-    vip: {
-        url: "https://cdn.discordapp.com/attachments/747410238944051271/748572846845984868/badge-crown.png",
-    },
-    gfx: {
-        url: "https://cdn.discordapp.com/attachments/747410238944051271/748576637225795614/badge-gfx.png",
-    },
-};
 
-var badgeImplemented = {
-    verified: false,
-    "content-creators": false,
-    dev: false,
-    vip: false,
-    gfx: false,
-};
+// Local module / file imports
+const rsData = require("./rsData.json");
+const { initDiscord } = require("./featureModules/rich-presence");
+const badgeManager = require("./featureModules/badges");
 
 // *** Do Some Stuff **
 ipcRenderer.on("Escape", () => {
@@ -111,35 +84,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 characterDataOldValue: true,
             });
 
-            // *** Badges Features ***
-
-            function checkPlayer() {
-                var checkDat = undefined
-                try {
-                    checkDat = window.getGameActivity()
-                } catch {}
-                if (checkDat !== undefined)
-                    badges.forEach((cur) => {
-                        if (badgeObj[cur].indexOf(window.getGameActivity().user) != -1) {
-                            if (!badgeImplemented[cur]) {
-                                document
-                                    .getElementsByClassName("menuClassPlayerName")[0]
-                                    .insertAdjacentHTML(
-                                        "beforebegin",
-                                        `<img style="height:50px" src="` + badgeUrls[cur].url + `">`
-                                    );
-                                badgeImplemented[cur] = true;
-                            }
-                        }
-                    });
-
-            }
-
-            // *** Alt Manager ***
+            // *** Player Name Observer for Badges ***
 
             new MutationObserver((mutations, observer) => {
                 if (document.getElementsByClassName("menuClassPlayerName")[0]) {
-                    checkPlayer();
+                    badgeManager.checkPlayer();
                 }
             }).observe(document, {
                 chatList: true,
