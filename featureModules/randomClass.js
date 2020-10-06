8484abe1bfd04781d62589ed39f647cdc77f094b
AryaveerSR <AryaveerSR@users.noreply.github.com>
2020-10-06 22:27:30 +0530

    Update randomClass.js

--- featureModules/randomClass.js
+++ featureModules/randomClass.js
@@ -1,7 +1,7 @@
 var { buttons, getID } = require("../consts");
 
 function addRandomClassButton() {
-    getID("menuClassContainer").insertAdjacentElement(
+    getID("menuClassContainer").insertAdjacentHTML(
         "beforeend",
         '<div id="randomClass customizeButton" class="button bigShadowT mycustomButton" onmouseenter="playTick()" onclick="window.randomClass()">Random Class</div>'
     );
