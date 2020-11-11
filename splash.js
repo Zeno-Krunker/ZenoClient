dfdb6e9b2611c5f0f6cca68adcfa1e2e6e4942fb
TheUltimateKeval <32948647+TheUltimateKeval@users.noreply.github.com>
2020-11-11 18:43:16 +0530

    Fixed Auto-Updater - Should work hopefully but needs to be tested on different platforms :<

--- splash.js
+++ splash.js
@@ -1,41 +1,46 @@
 const { ipcRenderer: ipcRenderer, remote } = require("electron");
-const http = require('http');
+const https = require('https');
 const fs = require('fs');
 
 // ** REMEMBER THIS EVERY UPDATE, JUST INCREASE +1, TO TEST, DECREASE -1 **
 
-const version = 18;
+const version = 17;
 
-document.addEventListener('DOMContentLoaded', async(event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
     var status = document.getElementById('bottom');
     var json;
-    console.log("before fetch");
-    await fetch("https://zenokrunkerapi.web.app/latestVersion.json")
+
+    fetch("https://zenokrunkerapi.web.app/latestVersion.json")
         .then((resp) => resp.json())
         .then((resp) => json = resp);
-    console.log("after fetch");
-    if (json.version > version) {
-        status.innerHTML = 'Update Found. Downloading Update..';
-        setTimeout(() => {
-            process.noAsar = !0;
-            download(`https://zenokrunkerapi.web.app/updates/${process.platform}/app.asar/`, __dirname, () => {
-                status.innerHTML = 'Update Downloaded. Restarting...';
-                setTimeout(() => {
-                    ipcRenderer.send('restart-client');
-                }, 2000);
-            });
-        }, 2000);
-    } else {
-        console.log("else statement");
-        status.innerHTML = 'No Update Found';
-        setTimeout(ipcRenderer.send('noUpdate'), 5000);
-    }
+
+    setTimeout(() => {
+        if (json && json.version > version) {
+            status.innerHTML = 'Update Found. Downloading Update..';
+            setTimeout(() => {
+                process.noAsar = !0;
+
+                let downloadURL = `https://zenokrunkerapi.web.app/updates/${process.platform}/v${json.version}.asar`;
+
+                download(downloadURL, __dirname + "/app.asar", () => {
+                    status.innerHTML = 'Update Downloaded. Restarting...';
+                    setTimeout(() => {
+                        ipcRenderer.send('restart-client');
+                    }, 2000);
+                });
+            }, 2000);
+        } else {
+            console.log("No Update Found");
+            status.innerHTML = 'No Update Found';
+            setTimeout(ipcRenderer.send('noUpdate'), 5000);
+        }
+    }, 10000);
 });
 
 
 var download = function(url, dest, cb) {
     var file = fs.createWriteStream(dest);
-    var request = http.get(url, function(response) {
+    var request = https.get(url, function(response) {
         response.pipe(file);
         file.on('finish', function() {
             file.close(cb);
