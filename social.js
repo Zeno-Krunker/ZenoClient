cb7ab9b384ff9f7316767c7410819bafab6f7dce
TheUltimateKeval <32948647+TheUltimateKeval@users.noreply.github.com>
2021-02-23 18:03:57 +0530

    Added right-click to open link in same window for social

--- social.js
+++ social.js
@@ -24,18 +24,21 @@ window.addEventListener("keydown", e => {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    (function() {
-        'use strict';
-        let cssFile = (store.get("RS") && fs.existsSync(getResourceSwapper(remote) + "css/social_custom.css")) ? getResourceSwapper(remote) + "css/social_custom.css" : __dirname + "/css/social/social.css";
-            fs.readFile(cssFile, "utf-8", (e, data) => {
-                if (e) return console.log(e);
-                document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", `<style id='custom-css'></style>`);
-                getID("custom-css").innerHTML = data;
-        });
+    let cssFile = (store.get("RS") && fs.existsSync(getResourceSwapper(remote) + "css/social_custom.css")) ? getResourceSwapper(remote) + "css/social_custom.css" : __dirname + "/css/social/social.css";
+        fs.readFile(cssFile, "utf-8", (e, data) => {
+            if (e) return console.log(e);
+            document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", `<style id='custom-css'></style>`);
+            getID("custom-css").innerHTML = data;
+    });
 
-        window.document.body.insertAdjacentHTML("beforeend", `<div id="hrefprev" style="position:absolute;left:0;bottom:0;height:15px;background-color:#fff;z-index:9999;font-family:sans-serif;font-size: 12px;line-height:15px;padding-left:5px;padding-right:5px"></div>`)
-        let hrefprev = getID("hrefprev");
+    window.document.body.insertAdjacentHTML("beforeend", `<div id="hrefprev" style="position:absolute;left:0;bottom:0;height:15px;background-color:#fff;z-index:9999;font-family:sans-serif;font-size: 12px;line-height:15px;padding-left:5px;padding-right:5px"></div>`)
+    let hrefprev = getID("hrefprev");
 
-        window.addEventListener("mousemove", e => { hrefprev.innerHTML = e.target?.href || "" });
-    })();
+    window.addEventListener("mousemove", e => { hrefprev.innerHTML = e.target?.href || "" });
+    window.addEventListener("mousedown", e => {
+        if(e.button == 2 && e.target.href && !e.target.disabled) {
+            e.preventDefault();
+            window.location.href = e.target.href;
+        }
+    });
 });
