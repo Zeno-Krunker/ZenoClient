9232ebb01390dfb3160b10d6ccaca47165fe3532
TheUltimateKeval <32948647+TheUltimateKeval@users.noreply.github.com>
2021-02-23 17:46:30 +0530

    Added URL Preview on Social Window

--- social.js
+++ social.js
@@ -21,7 +21,7 @@ window.addEventListener("keydown", e => {
         case "F7": ipcRenderer.send("devtools"); break;
         default: break;
     }
-})
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     (function() {
@@ -32,5 +32,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", `<style id='custom-css'></style>`);
                 getID("custom-css").innerHTML = data;
         });
+
+        window.document.body.insertAdjacentHTML("beforeend", `<div id="hrefprev" style="position:absolute;left:0;bottom:0;height:15px;background-color:#fff;z-index:9999;font-family:sans-serif;font-size: 12px;line-height:15px;padding-left:5px;padding-right:5px"></div>`)
+        let hrefprev = getID("hrefprev");
+
+        window.addEventListener("mousemove", e => { hrefprev.innerHTML = e.target?.href || "" });
     })();
 });
